f8e85ceae52c1ed9b5887d695f0e3afdfde35d73
Carlos Antônio <105462756+carlosaljuniorcg@users.noreply.github.com>
2023-08-02 09:31:30 -0300

    Now

--- src/app/componentes/livro/livro.component.ts
+++ src/app/componentes/livro/livro.component.ts
@@ -1,10 +1,36 @@
 import { Component, Input } from '@angular/core';
 import { Livro } from 'src/app/models/interfaces';
+import { trigger, transition, query, style, stagger, animate, keyframes } from '@angular/animations';
 
 @Component({
-  selector: 'app-livro',
+  selector: 'app-lista-livros',
   templateUrl: './livro.component.html',
-  styleUrls: ['./livro.component.css']
+  styleUrls: ['./livro.component.css'],
+  animations: [
+    trigger('listAnimation', [
+      transition('* <=> *', [
+        query(':enter', [
+          style({ opacity: 0, transform: 'translateY(-50px)' }),
+          stagger('100ms', [
+            animate('500ms ease-out', keyframes([
+              style({ opacity: 0, transform: 'translateY(-50px)', offset: 0 }),
+              style({ opacity: 0.5, transform: 'translateY(-25px)', offset: 0.3 }),
+              style({ opacity: 1, transform: 'none', offset: 1 }),
+            ])),
+          ]),
+        ], { optional: true }),
+        query(':leave', [
+          stagger('100ms', [
+            animate('500ms ease-out', keyframes([
+              style({ opacity: 1, transform: 'none', offset: 0 }),
+              style({ opacity: 0.5, transform: 'translateY(-25px)', offset: 0.3 }),
+              style({ opacity: 0, transform: 'translateY(-50px)', offset: 1 }),
+            ])),
+          ]),
+        ], { optional: true }),
+      ]),
+    ]),
+  ]
 })
 export class LivroComponent {
 
